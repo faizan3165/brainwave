513c6681ff743c7166223876ac9ae1a6b3834093
faizan3165 <faizanmehmood3165@gmail.com>
2024-03-05 21:52:07 +0500

    implement header component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components";
+
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Button from "./Button";
+
+
+export {Header, Button}
